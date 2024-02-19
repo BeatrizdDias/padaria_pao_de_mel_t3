96928e34cedfd310e3c8c06f94f2270a0833fd5a
BeatrizdDias <bdias6802@gmail.com>
2024-02-19 18:55:00 -0300

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react" //opcional
 import "./App.css" //Linkando o arquivo App.css 
 import Header from "../src/componentes/Header/Header.jsx"
 import Main from "./componentes/Main/Main.jsx"
+import Footer from "./componentes/Footer/Footer"
 
 function App() {
   return (
     <>   
   <Header />
   <Main />
-     </>
+  <Footer />
+    </>
 
 
   )
---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+
+
+function Footer() {
+
+    //setTimeout é um método que executa um bloco de código
+    // depois de um determinado tempo.
+    // estrutura do setTimeout()
+    // setTimeout(() => {}, tempo)
+    return (
+        <footer>
+            <h3>Mensagem do setTimeout</h3>
+            <button>Mensagem</button>
+        </footer>
+    )
+}
+
+export default Footer
